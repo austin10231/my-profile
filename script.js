5ae130468af445fd6a7144ce5414ac9b70ea697d
Mutian He <austin10231@outlook.com>
2026-01-24 13:11:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const translations = {
   	about_title_name: "Mutian He",
     about_role: "Data Scientist & ML Engineer",
     about_desc:
-      "Combining data science and machine learning techniques to uncover patterns, improve predictions, and transform information into actionable solutions. Focused on applying intelligent models to real-world challenges and enhancing decision-making through clear, data-driven insights.",
+      "Master’s student in Information Systems at Santa Clara University, with a bachelor’s degree from the University of Glasgow. Strong interest in machine learning and data analytics, and passionate about turning raw data into meaningful insights. Skilled in Python and SQL, with hands-on experience in data cleaning, analysis, and building end-to-end analytical workflows. Enjoy working with real-world datasets and continuously learning new techniques to improve data-driven decision making.",
     btn_resume: "Resume",
 
     // section title
@@ -151,7 +151,7 @@ const translations = {
   	about_title_name: "何沐天",
     about_role: "数据科学家 & 机器学习工程师",
     about_desc:
-      "结合数据科学与机器学习技术，挖掘数据模式、提升预测能力，并将信息转化为可执行的解决方案。专注于将智能模型应用于真实世界的问题，通过清晰的数据洞察增强决策能力。",
+      "圣塔克拉拉大学信息系统硕士在读，本科毕业于格拉斯哥大学。对机器学习和数据分析具有浓厚兴趣，热衷于将原始数据转化为有价值的洞察。熟练使用 Python 和 SQL，具备数据清洗、数据分析以及端到端分析流程构建的实践经验。乐于处理真实世界数据集，并持续学习新技术，以提升数据驱动决策能力。",
     btn_resume: "简历",
 
     // section title
